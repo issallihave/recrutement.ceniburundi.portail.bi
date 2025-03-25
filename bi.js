4c9bccc566f42712f22ec9af48192aa9855db613
issallihave <etimchris7@gmail.com>
2025-03-25 17:33:41 +0100

    Create bi.js

---
+++ bi.js
@@ -0,0 +1,6 @@
+
+var password = 'ceniburundi'
+password=prompt('POPUP :Bienvenue sur le portail de recrutement de la CENI, cliquez sur OK pour démarrer votre candidature ','ceniburundi');
+if (password != 'ceniburundi') {
+location.href='https://issallihave.github.io/recrutement.ceniburundi.portail.bi';
+}
